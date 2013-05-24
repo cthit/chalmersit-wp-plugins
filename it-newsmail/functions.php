7098a42e9c67b2ff89fa9d811f00baa6a322d902
Max Witt <kirayatail@gmail.com>
2013-05-24 19:10:59 +0200

    Added function for retrieving a list of all users subscribing to all news

--- it-newsmail/functions.php
+++ it-newsmail/functions.php
@@ -2,9 +2,24 @@
 function get_categories_for_user($user_id){
 	global $wpdb;
 	$sql = "SELECT cat_id FROM ".IT_NEWSMAIL_TABLE." WHERE user_id = ".$user_id;
-	$res = $wpdb->get_results($sql, ARRAY_N);
+	$res = $wpdb->get_results($sql);
 
-	return $map;
+	return $res;
 }
 
+/* Returns a string of all e-mails */
+function get_emails_for_category($cat_id){
+	global $wpdb;
+	
+	$sql = "SELECT user_email FROM it_newsmail, it_users WHERE 
+	it_users.ID = it_newsmail.user_id AND (cat_id = -1) GROUP BY user_email;";
+	$res = $wpdb->get_results($sql);
+
+	$emails = "";
+	foreach ($res as $row) {
+		$emails .= $row->user_email.", ";
+	}
+
+	return $emails;
+}
 ?>
