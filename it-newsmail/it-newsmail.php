42b3de16bd2df89aae4ebcc451623a711bd23c78
Max Witt <kirayatail@gmail.com>
2013-05-27 20:54:59 +0200

    Created a queueing function where post IDs are pushed onto a queue instead of mailed instantly.

--- it-newsmail/it-newsmail.php
+++ it-newsmail/it-newsmail.php
@@ -9,12 +9,14 @@
 */
 
 global $wpdb;
-define("IT_NEWSMAIL_TABLE", $wpdb->prefix. "newsmail");
+define("SUBSCRIBE_TABLE", $wpdb->prefix."newsmail");
+define("QUEUE_TABLE", $wpdb->prefix."newsmail_queue");
+define("DELAY_TIME", 10);
 
 register_activation_hook(__FILE__, 'it_newsmail_activate');
 register_deactivation_hook(__FILE__,'it_newsmail_deactivate');
 add_action("init", "itnm_register_subscription");
-add_action("publish_post", "itnm_doMail");
+add_action("publish_post", "itnm_queuePost");
 
 require_once "class.ITNewsMail_Widget.php";
 require_once "functions.php";
@@ -37,11 +39,11 @@ function itnm_register_subscription(){
 		foreach ($old_cats as $key => $value) {
 			if($old_cats[$key]['choice'] && !$new_cats[$key]['choice']){
 				// Something removed, perform delete!
-				$wpdb->query($wpdb->prepare("DELETE FROM ".IT_NEWSMAIL_TABLE." WHERE user_id = %d AND cat_id = %d", $user_id, $key));
+				$wpdb->query($wpdb->prepare("DELETE FROM ".SUBSCRIBE_TABLE." WHERE user_id = %d AND cat_id = %d", $user_id, $key));
 			}
 			if(!$old_cats[$key]['choice'] && $new_cats[$key]['choice']){
 				// Something added, perform insert!
-				$wpdb->insert(IT_NEWSMAIL_TABLE, array(
+				$wpdb->insert(SUBSCRIBE_TABLE, array(
 					'user_id' => $user_id,
 					'cat_id' => $key));
 			}
@@ -53,22 +55,43 @@ function itnm_register_subscription(){
 function it_newsmail_activate(){
 	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 
-	$sql = "CREATE TABLE IF NOT EXISTS ".IT_NEWSMAIL_TABLE." (
-		id mediumint(9) NOT NULL AUTO_INCREMENT,
+	/* Subscribe table */
+	$sql = "CREATE TABLE IF NOT EXISTS ".SUBSCRIBE_TABLE." (
 		user_id bigint(20) unsigned NOT NULL,
 		cat_id bigint(20) signed NOT NULL,
 
-		PRIMARY KEY (id)
+		PRIMARY KEY (user_id, cat_id)
 		);";
 	
+	dbDelta($sql);
+
+	/* Queue table */
+	$sql = "CREATE TABLE IF NOT EXISTS ".QUEUE_TABLE." (
+		post_id bigint(20) unsigned NOT NULL,
+		minutestamp bigint(20) unsigned NOT NULL,
+
+		PRIMARY KEY (post_id)
+		);";
+
 	dbDelta($sql);
 }
 
 function it_newsmail_deactivate(){
 	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 
-	$sql = "DROP TABLE ".IT_NEWSMAIL_TABLE.";";
+	$sql = "DROP TABLE ".SUBSCRIBE_TABLE.";";
 	dbDelta($sql);
+}
+
+function itnm_queuePost($post_id) {
+	global $wpdb;
+	$minutes = intval(current_time('timestamp')/60)+DELAY_TIME;
+
+	// Smart insert which updates if the post already exists
+	$sql = $wpdb->prepare("INSERT INTO ".QUEUE_TABLE." (post_id, minutestamp) 
+		VALUES (%d, %d) ON DUPLICATE KEY UPDATE 
+		minutestamp = %d", $post_id, $minutes, $minutes);
+	$wpdb->query($sql);
 }
 
 function itnm_doMail($post_id){
@@ -92,7 +115,7 @@ function itnm_doMail($post_id){
 	$recipients = "";
 	
 	/* The following block is for dispatching
-	 * one mail per 89 users, as we have a limit
+	 * one mail per 90 users, as we have a limit
 	 * on recipients per mail
 	 */
 	for ($i=0; $i<count($allrecipients);$i++) { 
