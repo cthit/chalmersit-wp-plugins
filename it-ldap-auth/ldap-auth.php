9fbf301ce17d80a31a794f07de9753e6fd1f2476
Johan Lindskogen <johan.lindskogen@gmail.com>
2013-10-08 02:32:30 +0200

    Neater way of overriding set password. Directly communicating with auth-api

--- it-ldap-auth/ldap-auth.php
+++ it-ldap-auth/ldap-auth.php
@@ -25,6 +25,16 @@ define("BASE_PATH", "https://chalmers.it/auth/");
 
 define("IT_LDAP_ACTION", BASE_PATH . "login.php");
 define("IT_LDAP_ACTION_LOGOUT", BASE_PATH . "logout.php");
+define("IT_LDAP_ACTION_RESET", BASE_PATH . "resetpass.php");
+
+// add_action('retrieve_password', 'lost_password', 10, 1);
+
+//show_user_profile
+//edit_user_profile
+
+function lost_password($cid) {
+	wp_remote_post(BASE_PATH."resetpass.php", array("username" => $cid, "no-redirect" => true));
+}
 
 if (!function_exists("wp_validate_auth_cookie")) {
 	function wp_validate_auth_cookie() {
@@ -44,3 +54,24 @@ if (!function_exists("wp_validate_auth_cookie")) {
 		return $user->ID;
 	}
 }
+
+if ( !function_exists('wp_set_password') ) :
+/**
+ * Updates the user's password with a new encrypted one.
+ *
+ * For integration with other applications, this function can be overwritten to
+ * instead use the other package password checking algorithm.
+ *
+ * @since 2.5
+ * @uses $wpdb WordPress database object for queries
+ * @uses wp_hash_password() Used to encrypt the user's password before passing to the database
+ *
+ * @param string $password The plaintext new user password
+ * @param int $user_id User ID
+ */
+function wp_set_password( $password, $user_id ) {
+	$token = $_COOKIE[COOKIE_NAME];
+	wp_remote_post(BASE_PATH."resetpass.php", array("password" => $password, "cookie" => $token));
+	wp_cache_delete($user_id, 'users');
+}
+endif;
